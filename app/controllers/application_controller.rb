e6fa9854c2bfc20b42bdb69babdeb821d6376d40
Israel De Castro A <fulanito203@gmail.com>
2023-02-20 11:50:51 -0400

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :name, :email, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
+
+
+end
