1237dfcadfb8c2ca77a3adc728bc4883b6749a2b
Israel De Castro A <fulanito203@gmail.com>
2023-02-24 08:44:18 -0400

    Finished

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -2,9 +2,16 @@ import "./main.scss";
 
 import { createApp } from "vue";
 import Blogs from "../components/views/Blogs.vue";
+import SinglePost from "../components/views/SinglePost.vue";
 
 const blogsElement = document.querySelector("#blogs");
 if (blogsElement) {
   const blog = createApp(Blogs);
   blog.mount(blogsElement);
+}
+
+const singlePost = document.querySelector("#singlePost");
+if (singlePost) {
+  const post = createApp(SinglePost);
+  post.mount(singlePost);
 }
